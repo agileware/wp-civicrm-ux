fac0099ad27df88b5ee1c21bef6fc6e5647d6ca1
Pengyi Zhang <pengyi@agileware.com.au>
2019-07-12 13:55:08 +1000

    WPCIVIUX-6 increment version 1.0.1

--- civicrm-ux.php
+++ civicrm-ux.php
@@ -9,7 +9,7 @@
  * Plugin Name:       CiviCRM UX
  * Plugin URI:        https://agileware.com.au
  * Description:       UX for CiviCRM
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Agileware
  * Author URI:        https://agileware.com.au/
  * License:           GPL-2.0+
@@ -24,7 +24,7 @@
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CIVICRM_UXVERSION', '1.0.0' );
+define( 'CIVICRM_UXVERSION', '1.0.1' );
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
