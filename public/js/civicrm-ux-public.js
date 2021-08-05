5e424fbed5ee8a744a4fa2ac944df019bea1794c
Francis Whittle <francis@agileware.com.au>
2021-08-05 14:28:47 +1000

    PROJ-1885 Ensure than recurring options are enabled.

--- public/js/civicrm-ux-public.js
+++ public/js/civicrm-ux-public.js
@@ -2,8 +2,9 @@
     'use strict';
 
     $(function() {
-        if(wp.CiviCRM_UX.is_recur_default) {
-            jQuery(':input[name=is_recur]').prop('checked', 'checked')
+        if(wp.CiviCRM_UX.is_recur_default && !jQuery(':input[name=is_recur]').prop('disabled')) {
+            jQuery('.is_recur-section :input').prop('disabled', false);
+            jQuery(':input[name=is_recur]').prop('checked', 'checked');
         }
     });
 
