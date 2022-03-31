b2f9aa14faf1465c710776709c147212910597ef
Francis Whittle <francis@agileware.com.au>
2022-03-31 14:27:35 +1100

    Version 1.9.4

--- civicrm-ux.php
+++ civicrm-ux.php
@@ -9,7 +9,7 @@
  * Plugin Name:       WP CiviCRM UX
  * Plugin URI:        https://github.com/agileware/wp-civicrm-ux
  * Description:       A better user experience for integrating WordPress and CiviCRM
- * Version:           1.9.3
+ * Version:           1.9.4
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Agileware
