352c138d94d2176168e4ebbaae55ed84f015c980
Aidan Saunders <aidan.saunders@squiffle.uk>
2022-06-21 14:38:36 +0100

    Fix php notice "Notice: Accessing static property Civicrm_Ux::$directory as non static in ..."

--- includes/class-civicrm-ux.php
+++ includes/class-civicrm-ux.php
@@ -102,7 +102,7 @@ class Civicrm_Ux {
 	private function __construct( $plugin_file ) {
 		$this->version = \get_file_data( realpath( $plugin_file ), [ 'Version' => 'Version' ], 'plugin' )['Version'];
 		$this->civicrm_ux = 'civicrm-ux';
-        $this->directory = plugin_dir_path( $plugin_file );
+        self::$directory = plugin_dir_path( $plugin_file );
 
 		$this->load_dependencies();
 		$this->set_locale();
@@ -113,8 +113,8 @@ class Civicrm_Ux {
 		$this->define_rest();
 		$this->define_magic_tag();
 
-        if( !function_exists( 'urlparam' ) && !empty( $this->directory ) ) {
-            include_once( $this->directory . '/packaged/url-params/urlparams.php');
+        if( !function_exists( 'urlparam' ) && !empty( self::$directory ) ) {
+            include_once( self::$directory . '/packaged/url-params/urlparams.php');
         }
 
         $this->loader->add_action( 'init', $this, 'civicrm_init' );
