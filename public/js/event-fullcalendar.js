9d4d931f8f81fdf461a8f564772ca60fe5fc4491
Agileware <david@agileware.com.au>
2022-12-12 15:49:19 +1100

    Removed explicit paths to FCVIC site (incl. images, ajax URL & iCal feed) (now passed in via wp_localize_script). Still depends on iCal feed being present in uploads directory (or, could use a generated iCal link)

--- public/js/event-fullcalendar.js
+++ public/js/event-fullcalendar.js
@@ -9,13 +9,13 @@ const domevent = function(eventName, detail) {
 };
 
 function formatAMPM(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours ? hours : 12;
   minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
+  const strTime = hours + ':' + minutes + ' ' + ampm;
   return strTime;
 }
 
@@ -23,7 +23,7 @@ function extractUrlValue(key, url)
 {
     if (typeof(url) === 'undefined')
         url = window.location.href;
-    var match = url.match('[?&]' + key + '=([^&]+)');
+    const match = url.match('[?&]' + key + '=([^&]+)');
     return match ? match[1] : null;
 }
 
@@ -32,23 +32,18 @@ const months = ['January','February','March','April','May','June','July','August
 
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('civicrm-event-fullcalendar');
-    var sources_api_obj = {};
-    var imageFetched = 0;
-
-    
-
-    //
+    let sources_api_obj = {};
 
     const sources = {
         meetings: {
-            url: "/fcvic_sandbox/wp-content/uploads/meetings.ics",
+            url: wp_site_obj.upload.baseurl + '/' + wp_site_obj.meetings,
             format: "ics",
             category: "meeting",
             color: "red",
             textColor: "white"
         },
         fundraiser: {
-            url: "/fcvic_sandbox/wp-content/uploads/fundraisers.ics", 
+            url: wp_site_obj.upload.baseurl + '/' + wp_site_obj.fundraisers, 
             format: "ics",
             category: "fundraiser",
             color: "yellow",
@@ -90,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         firstDay: 1,
         eventContent: function(arg) {
-            var sortByTypeBtn = document.querySelector('.fc-sortByType-button');
+            let sortByTypeBtn = document.querySelector('.fc-sortByType-button');
             if (sortByTypeBtn) {
                 sortByTypeBtn.remove();
             }
@@ -99,26 +94,24 @@ document.addEventListener('DOMContentLoaded', function() {
             eventHolder.classList.add('event-holder');
             eventHolder.style.backgroundColor = arg.backgroundColor;
             eventHolder.style.color = arg.textColor;
-            eventHolder.innerHTML = '<div class="fc-event-time">' + arg.timeText + '</div><div class="fc-event-title">' + arg.event.title + '</div>';
+            eventHolder.innerHTML = '<div class="fc-event-time">' + arg.timeText + '</div><div class="fc-event-title"><a style="text-decoration: none; color: ' + arg.textColor + ';" href="' + arg.event.url + '">' + arg.event.title + '</a></div>';
 
             let arrayOfDomNodes = [ eventHolder ]
             return { domNodes: arrayOfDomNodes }
         },
 
         eventDidMount: function(info) {
-            var event_img;
-            var event_title;
-            var event_time;
-            var event_location;
+            let event_img;
+            let event_title;
+            let event_time;
+            let event_location;
 
             const event_id = extractUrlValue('id', info.event.url);
-
-            console.log(info);
 
             jQuery.ajax({
                 method : "GET",
                 dataType : "json",
-                url : my_ajax_object.ajax_url,
+                url : wp_site_obj.ajax_url,
                 data : {action: "get_thumbnail", event_id : event_id},
                 success: function(response) {
 
@@ -127,22 +120,20 @@ document.addEventListener('DOMContentLoaded', function() {
                     const event_start = new Date(response.result['start_date']);
                     const event_end = new Date(response.result['end_date']);
 
-
                     event_time = formatAMPM(event_start) + ' to ' + formatAMPM(event_end);
-                    event_img = '/fcvic_sandbox/wp-content/uploads/civicrm/custom/' + response.result['file.uri'];
+                    event_img = wp_site_obj.upload.baseurl + '/civicrm/custom/' + response.result['file.uri'];
                     event_title = response.result.title;
                     event_location = response.result['address.city'] + ' ' + response.result['address.country_id:name'];
 
-                    var template = `<div class="tooltip">
-                        <div id="event-name">` + info.event.title + `</div>`;
-
-                    template += response.result['file.uri'] ? '<img id="event-img" src="' + event_img + '">' : '';
-                    template += `<div id="event-time"><i class="fa fa-clock-o"></i><span id="event-time-text"> ` + event_time + `</span></div>
-                        <div id="event-location"><i class="fa fa-map-marker-alt"></i><span id="event-location-text">` + event_location + `</span></div>
+                    const template = `<div class="tooltip">
+                        <div id="event-name">${info.event.title}</div>
+                        ${response.result['file.uri'] ? '<img id="event-img" src="' + event_img + '">' : ''}
+                        <div id="event-time"><i class="fa fa-clock-o"></i><span id="event-time-text">${event_time}</span></div>
+                        <div id="event-location"><i class="fa fa-map-marker-alt"></i><span id="event-location-text">${event_location}</span></div>
                         </div>`
                        
 
-                    var tooltip = new Tooltip(info.el, {
+                    let tooltip = new Tooltip(info.el, {
                         html: true,
                         template: template,
                         title: info.event.title,
@@ -157,42 +148,40 @@ document.addEventListener('DOMContentLoaded', function() {
         },
 
         eventClick: function(eventClickInfo) {
-            var jsEvent = eventClickInfo.jsEvent;
+            let jsEvent = eventClickInfo.jsEvent;
             jsEvent.preventDefault();
 
             const event_id = extractUrlValue('id', eventClickInfo.event.url);
 
-
             jQuery.ajax({
                 method : "GET",
                 dataType : "json",
-                url : my_ajax_object.ajax_url,
+                url : wp_site_obj.ajax_url,
                 data : {action: "get_event_info", event_id : event_id},
                 success: function(response) {
-                    var header = document.getElementById("civicrm-ux-event-popup-header");
-                    var summary = document.getElementById("civicrm-ux-event-popup-summary");
-                    var desc = document.getElementById("civicrm-ux-event-popup-desc");
-                    var img = document.getElementById("civicrm-ux-event-popup-img");
-                    var event_type = document.getElementById("civicrm-ux-event-popup-eventtype");
-                    var event_loc = document.getElementById("civicrm-ux-event-popup-location");
-                    var event_time_day = document.getElementById("civicrm-ux-event-popup-time-day");
-                    var event_time_hours = document.getElementById("civicrm-ux-event-popup-time-hours");
+                    let header = document.getElementById("civicrm-ux-event-popup-header");
+                    let summary = document.getElementById("civicrm-ux-event-popup-summary");
+                    let desc = document.getElementById("civicrm-ux-event-popup-desc");
+                    let img = document.getElementById("civicrm-ux-event-popup-img");
+                    let event_type = document.getElementById("civicrm-ux-event-popup-eventtype");
+                    let event_loc = document.getElementById("civicrm-ux-event-popup-location");
+                    let event_time_day = document.getElementById("civicrm-ux-event-popup-time-day");
+                    let event_time_hours = document.getElementById("civicrm-ux-event-popup-time-hours");
 
                     const event_start = new Date(response.result['start_date']);
                     const event_end = new Date(response.result['end_date']);
 
-
                     header.innerHTML = response.result['title'];
                     summary.innerHTML = response.result['summary'];
                     desc.innerHTML = response.result['description'];
-                    img.src = '/fcvic_sandbox/wp-content/uploads/civicrm/custom/' + response.result['file.uri'];
+                    img.src = wp_site_obj.upload.baseurl + '/civicrm/custom/' + response.result['file.uri'];
                     event_type.innerHTML = response.result['event_type_id:name'];
                     event_time_day.innerHTML = days[event_start.getDay()] + ', ' + event_start.getDate() + ' ' + months[event_start.getMonth()] + ' ' + event_start.getFullYear();
                     event_time_hours.innerHTML = formatAMPM(event_start) + ' to ' + formatAMPM(event_end);
                 }
             })
 
-            var popup = document.getElementById("civicrm-ux-event-popup");
+            let popup = document.getElementById("civicrm-ux-event-popup");
             calendarEl.style.display = "none";
             popup.style.display = "block";
         }
@@ -203,7 +192,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const calendar = new FullCalendar.Calendar(calendarEl, calendarParams);
 
     let sources_tmp = calendar.getEventSources();
-    for (var i = 0; i < sources_tmp.length; i++) {
+    for (let i = 0; i < sources_tmp.length; i++) {
         let source = sources_tmp[i];
         sources_api_obj[source.internalEventSource.extendedProps.category] = source;
     }
@@ -211,9 +200,9 @@ document.addEventListener('DOMContentLoaded', function() {
     calendarEl.dispatchEvent(domevent('fullcalendar:prerender'));
     calendar.render();
 
-    var sortByTypeBtn = document.querySelector('.fc-sortByType-button');
-    var parent = sortByTypeBtn.parentElement;
-    var sortByTypeSelect = document.createElement('select');
+    let sortByTypeBtn = document.querySelector('.fc-sortByType-button');
+    let parent = sortByTypeBtn.parentElement;
+    let sortByTypeSelect = document.createElement('select');
     sortByTypeSelect.classList.add('fc-button');
     sortByTypeSelect.classList.add('fc-button-primary');
     sortByTypeSelect.innerHTML = `<option selected="" value="all">sort by type</option>
@@ -231,10 +220,10 @@ document.addEventListener('DOMContentLoaded', function() {
         let val = selector.value;
 
         if (!(val == "all")) {
-            var sources = calendar.getEventSources();
-            var foundType = false;
-            for (var i = 0; i < sources.length; i++) {
-                var source = sources[i];
+            let sources = calendar.getEventSources();
+            let foundType = false;
+            for (let i = 0; i < sources.length; i++) {
+                let source = sources[i];
                 let cat = source.internalEventSource.extendedProps.category
                 if (cat != val) {
                     source.remove();
@@ -247,21 +236,17 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
         } else {
-            for (var key in sources_api_obj) {
+            for (let key in sources_api_obj) {
                 if (sources_api_obj.hasOwnProperty(key)) {
                     calendar.addEventSource(sources_api_obj[key]);
                 }
             }
         }
 
-
-        
         calendar.refetchEvents();
     });
 
-
-
-    var event_popup_x = document.getElementById("civicrm-ux-event-popup-close");
+    let event_popup_x = document.getElementById("civicrm-ux-event-popup-close");
     if (event_popup_x) {
         event_popup_x.addEventListener("click", function(){
             document.getElementById("civicrm-ux-event-popup").style.display = "none";
--- shortcodes/event/event-fullcalendar.php
+++ shortcodes/event/event-fullcalendar.php
@@ -19,13 +19,16 @@ class Civicrm_Ux_Shortcode_Event_FullCalendar extends Abstract_Civicrm_Ux_Shortc
 		wp_enqueue_script( 'ical', 'https://cdnjs.cloudflare.com/ajax/libs/ical.js/1.4.0/ical.js', [] );
 		wp_enqueue_script( 'fullcalendar-base', 'https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.9.0/main.js', [] );
 		wp_enqueue_script( 'popper', 'https://unpkg.com/popper.js/dist/umd/popper.min.js', [] );
-		wp_enqueue_script( 'tooltip', 'https://unpkg.com/tooltip.js/dist/umd/tooltip.min.js', [] );
+		wp_enqueue_script( 'tooltip', '/wp-content/uploads/tooltip.js', [] );
 		wp_enqueue_script( 'fullcalendar-ical', 'https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@5.9.0/main.global.js', [ 'fullcalendar-base' ] );
 		wp_enqueue_style( 'fullcalendar-styles', 'https://cdn.jsdelivr.net/npm/fullcalendar@5.9.0/main.min.css', [] );
 		wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', [] );
 		wp_enqueue_script( 'ux-fullcalendar', plugin_dir_url( dirname( __FILE__ ) . '/../../..' ) . 'public/js/event-fullcalendar.js', [ 'fullcalendar-ical' ], '1.7.0' );
-		wp_localize_script( 'ux-fullcalendar', 'my_ajax_object',
-            array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+		wp_localize_script( 'ux-fullcalendar', 'wp_site_obj',
+            array( 'ajax_url' => admin_url( 'admin-ajax.php'), 
+            	'upload' => wp_upload_dir(),
+            	'meetings' => 'meetings.ics', 
+            	'fundraisers' => 'fundraisers.ics'));
 
 
 
