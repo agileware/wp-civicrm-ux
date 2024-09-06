29094652ae9a8d9209bd5f913fe7583e042897ca
Iris Abarquez <iris@agileware.com.au>
2024-09-06 15:21:39 +1000

    WPCIVIUX-162 Add message if checksum url is invalid or expired

--- shortcodes/self-serve-checksum/self-serve-checksum.php
+++ shortcodes/self-serve-checksum/self-serve-checksum.php
@@ -17,6 +17,7 @@ class Civicrm_Ux_Shortcode_Self_Serve_Checksum extends Abstract_Civicrm_Ux_Short
 	 * @return mixed Should be the html output of the shortcode
 	 */
 	public function shortcode_callback( $atts = [], $content = null, $tag = '' ) {
+		$isValid = false;
 		// IF there is a valid CID and checksum in the  URL, display the content inside the shortcode
 		if ( isset( $_GET['cid'] ) && !empty( $_GET['cid'] ) && isset( $_GET['cid'] ) && !empty( $_GET['cid'] ) ) {
 			$cid = $_GET['cid'];
@@ -53,11 +54,13 @@ class Civicrm_Ux_Shortcode_Self_Serve_Checksum extends Abstract_Civicrm_Ux_Short
 			->get_store()
 			->get_option('self_serve_checksum');
 		
+		$errorMessage = !$isValid ? '<p>That link has expired or is invalid. Please request a new link below.</p>' : '';
 		$formText = wpautop( $self_serve_checksum['form_text'] );
 
         ob_start();
         ?>
 
+		<?php echo $errorMessage; ?>
 		<?php echo $formText; ?>
         <form id="ss-cs-form" method="post">
             <label for="ss-cs-email">Your email:</label>
