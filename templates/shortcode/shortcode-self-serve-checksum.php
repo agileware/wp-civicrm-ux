b9b2ca0eac87129521337ba988f1e8ddc737a2e2
Iris Abarquez <iris@agileware.com.au>
2025-04-17 16:34:25 +1000

    WPCIVIUX-184 Improve Self Serve Checksum form default template part markup and styling

--- templates/shortcode/shortcode-self-serve-checksum.php
+++ templates/shortcode/shortcode-self-serve-checksum.php
@@ -5,31 +5,57 @@
  * To customise, copy this file into your theme and make changes as needed.
  */
 
+if (
+    ! $args['form_submitted'] ||
+    ( $args['form_submitted'] && isset( $args['turnstile_passed'] ) && ! $args['turnstile_passed'] )
+) :
+?>
+    <section class="ss-cs-form" aria-label="Protected Content Access Request Form">
+        <div class="ss-cs-form-text">
+            <?php echo wp_kses_post( $args['form_text'] ); ?>
+        </div>
+        <form id="ss-cs-form" method="post" data-turnstilepassed="<?php echo esc_attr( $args['turnstile_passed'] ? 'true' : 'false' ); ?>" novalidate>
+            <fieldset>
+                <legend class="screen-reader-text">Enter your email to receive your unique link</legend>
 
-if ( !$args['form_submitted'] || 
-        ( $args['form_submitted'] && isset( $args['turnstile_passed'] ) && !$args['turnstile_passed'] ) ) {
-    
-    echo $args['form_text']; ?>
-    <form id="ss-cs-form" method="post" data-turnstilepassed="<?php echo $args['turnstile_passed'] ? 'true' : 'false'; ?>">
-        <label for="ss-cs-email">Your email:</label>
-        <input type="email" id="ss-cs-email" name="ss-cs-email" required>
-        <input type="hidden" name="ss-cs-title" value="<?php echo get_the_title(); ?>">
-        <input type="hidden" name="ss-cs-url" value="<?php echo $args['url']; ?>">
-        <?php echo $args['turnstile']; ?>
-        <button type="submit" name="ss-cs-submit" id="ss-cs-submit">Submit</button>
-    </form>
-    <?php
-    if ( !empty($args['turnstile']) ) { ?>
-    <script>
-        let turnstileCompleted = false;
-        
-        // This function will be called when Turnstile completes successfully
-        function onTurnstileComplete(token) {
-            turnstileCompleted = true;
+                <label for="ss-cs-email" class="ss-cs-email">Your email address:</label>
+                <input 
+                    type="email" 
+                    id="ss-cs-email" 
+                    name="ss-cs-email" 
+                    required 
+                    autocomplete="email" 
+                    aria-describedby="ss-cs-email-desc"
+                >
+                <p id="ss-cs-email-desc" class="screen-reader-text">Enter the email address you used to register.</p>
 
-            let submitButton = document.getElementById('ss-cs-submit');
-            submitButton.disabled = false;
-        }
-    </script>
-    <?php } ?>
-<?php } ?>
+                <input type="hidden" name="ss-cs-title" value="<?php echo esc_attr( get_the_title() ); ?>">
+                <input type="hidden" name="ss-cs-url" value="<?php echo esc_url( $args['url'] ); ?>">
+
+                <?php echo $args['turnstile']; ?>
+
+                <button 
+                    type="submit" 
+                    name="ss-cs-submit" 
+                    id="ss-cs-submit" 
+                    <?php echo ! empty( $args['turnstile'] ) ? 'disabled aria-disabled="true"' : ''; ?>
+                >
+                    Submit
+                </button>
+            </fieldset>
+        </form>
+    </section>
+    <?php if ( ! empty( $args['turnstile'] ) ) : ?>
+        <script>
+            let turnstileCompleted = false;
+            
+            // This function will be called when Turnstile completes successfully
+            function onTurnstileComplete(token) {
+                turnstileCompleted = true;
+
+                let submitButton = document.getElementById('ss-cs-submit');
+                submitButton.disabled = false;
+            }
+        </script>
+    <?php endif; ?>
+<?php endif; ?>
