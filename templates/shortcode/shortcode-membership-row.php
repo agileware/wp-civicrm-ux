683dd808f7b4d0d8128a45a2aa97290c43188338
Justin Freeman <justin@agileware.com.au>
2026-07-04 06:49:56 +1000

    H115959 - Remove class is-style-outline from renewal button link which was causing visibility issues in some WordPress themes

--- templates/shortcode/shortcode-membership-row.php
+++ templates/shortcode/shortcode-membership-row.php
@@ -56,7 +56,7 @@ foreach ($args['memberships'] as $membership) {
         <td><?= esc_html($formattedEndDate); ?></td>
         <td>
             <?php if ($renewalUrl && !empty($membership['end_date']) && (($expiration_offset_date && $endDate->getTimestamp() <= $expiration_offset_date) || $endDate->getTimestamp() <= time())) { ?>
-                <div class="wp-block-button is-style-outline">
+                <div class="wp-block-button">
                     <a href="<?= esc_url($renewalUrl); ?>" target="_blank" class="wp-block-button__link wp-element-button">Renew this membership</a>
                 </div>
             </td>
